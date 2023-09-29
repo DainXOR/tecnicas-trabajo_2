5424f0586698991d9263ee243e19b93678c576d7
carlosforeroudea <carlos.forero@udea.edu.co>
2023-09-29 12:55:56 -0500

    Se genera formulario login generico.

--- src/main/java/udea/tecnicas/view/HelloController.java
+++ src/main/java/udea/tecnicas/view/HelloController.java
@@ -2,6 +2,15 @@ package udea.tecnicas.view;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.stage.Modality;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -10,5 +19,18 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("TemplateLogin.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setTitle("ABC");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        }
+        catch(IOException exeption){
+            System.out.println(exeption.toString());
+        }
     }
 }
