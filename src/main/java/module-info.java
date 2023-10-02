f7fdf29b1549646dcf99db86b0d2fdfeec847f0d
carlosforeroudea <carlos.forero@udea.edu.co>
2023-10-02 15:52:38 -0500

    Concepto de navegacion

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,13 @@ module udea.tecnicas.view {
     requires javafx.controls;
     requires javafx.fxml;
 
+
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
+    requires com.gluonhq.charm.glisten;
+
 
     opens udea.tecnicas.view to javafx.fxml;
     exports udea.tecnicas.view;
--- src/main/java/udea/tecnicas/view/TemplateMenu.java
+++ src/main/java/udea/tecnicas/view/TemplateMenu.java
@@ -1,17 +1,14 @@
 package udea.tecnicas.view;
 //import com.gluonhq.charm.glisten.*;
+
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.LoadException;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-import org.kordamp.bootstrapfx.scene.layout.Panel;
-
-import java.io.IOException;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import com.gluonhq.charm.glisten.control.Icon;
 
 public class TemplateMenu {
     @FXML
@@ -20,16 +17,50 @@ public class TemplateMenu {
     private Pane Panel2;
     @FXML
     private AnchorPane Contenido;
+    @FXML
+    private Rectangle RectanguloMenu10;
+    @FXML
+    private Rectangle RectanguloMenu11;
+    @FXML
+    private Rectangle RectanguloMenu20;
+    @FXML
+    private Rectangle RectanguloMenu21;
+    @FXML
+    private Group GroupMenu10;
+    @FXML
+    private Group GroupMenu11;
+    @FXML
+    private Group GroupMenu20;
+    @FXML
+    private Group GroupMenu21;
 
 
 
     @FXML
     protected void MenuLicenciasClick(){
-
         hideoption1();
+    }
+    @FXML
+    protected void MenuClic(MouseEvent event){
+        Rectangle RectangleClicked =(Rectangle)event.getSource();
+        ResaltarMenu(RectangleClicked.getId());
 
-
-
+    }
+    private void ResaltarMenu(String idRectangle){
+        Color color_activo = Color.rgb(226,226,226);
+        Color color_inactivo = Color.rgb(255,255,255);
+        if(idRectangle.contains("RectanguloMenu1")){
+            if (idRectangle.equals("RectanguloMenu10"))
+            {
+                RectanguloMenu10.setFill(color_activo);
+                RectanguloMenu11.setFill(color_inactivo);
+            }
+            if (idRectangle.equals("RectanguloMenu11"))
+            {
+                RectanguloMenu11.setFill(color_activo);
+                RectanguloMenu10.setFill(color_inactivo);
+            }
+        }
     }
 
     private void hideoption1(){
